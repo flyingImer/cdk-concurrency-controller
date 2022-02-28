c919e28becd26f2bbdc21f9f2b3c210df1a5fd7e
flyingImer <ej.wang.devs@gmail.com>
2022-02-28 00:20:00 +0000

    chore: start with monocdk

--- .projenrc.js
+++ .projenrc.js
@@ -1,15 +1,38 @@
-const { awscdk } = require('projen');
+const { awscdk, DependencyType } = require('projen');
+
+const CDK_VERSION = '1.145.0';
+const CONSTRUCT_VERSION = '3.3.69';
+
 const project = new awscdk.AwsCdkConstructLibrary({
-  author: 'flyingImer',
+  author: 'Ej Wang',
   authorAddress: 'ej.wang.devs@gmail.com',
-  cdkVersion: '2.1.0',
+
+  // stick to monocdk for now
+  cdkVersion: CDK_VERSION,
+  cdkAssertions: false,
+  cdkAssert: false,
+  cdkDependenciesAsDeps: false,
+
   defaultReleaseBranch: 'main',
   name: 'cdk-concurrency-controller',
   repositoryUrl: 'https://github.com/flyingImer/cdk-concurrency-controller.git',
+  stability: 'experimental',
 
   // deps: [],                /* Runtime dependencies of this module. */
-  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
-  // devDeps: [],             /* Build dependencies for this module. */
+  description: 'Controlling concurrency in your distributed systems.', /* The description is just a string that helps people understand the purpose of the package. */
+  devDeps: [
+    `monocdk@${CDK_VERSION}`,
+    `@monocdk-experiment/assert@${CDK_VERSION}`,
+    `constructs@${CONSTRUCT_VERSION}`,
+  ], /* Build dependencies for this module. */
+  peerDeps: [
+    `monocdk@^${CDK_VERSION}`,
+  ],
   // packageName: undefined,  /* The "name" in package.json. */
 });
+// remove all @aws-cdk deps to adopt monocdk
+project.deps.all.filter(dep => dep.name.includes('@aws-cdk')).forEach(dep => project.deps.removeDependency(dep.name));
+project.deps.removeDependency('constructs', DependencyType.PEER);
+project.deps.addDependency(`constructs@^${CONSTRUCT_VERSION}`, DependencyType.PEER);
+
 project.synth();
---
+++ API.md
@@ -0,0 +1,39 @@
+# API Reference <a name="API Reference" id="api-reference"></a>
+
+
+
+## Classes <a name="Classes" id="Classes"></a>
+
+### Hello <a name="Hello" id="cdk-concurrency-controller.Hello"></a>
+
+#### Initializers <a name="Initializers" id="cdk-concurrency-controller.Hello.Initializer"></a>
+
+```typescript
+import { Hello } from 'cdk-concurrency-controller'
+
+new Hello()
+```
+
+| **Name** | **Type** | **Description** |
+| --- | --- | --- |
+
+---
+
+#### Methods <a name="Methods" id="Methods"></a>
+
+| **Name** | **Description** |
+| --- | --- |
+| <code><a href="#cdk-concurrency-controller.Hello.sayHello">sayHello</a></code> | *No description.* |
+
+---
+
+##### `sayHello` <a name="sayHello" id="cdk-concurrency-controller.Hello.sayHello"></a>
+
+```typescript
+public sayHello(): string
+```
+
+
+
+
+
